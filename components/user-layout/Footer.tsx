b831e3b8f0a4d29e7c5b8b153f52f04cf1db8391
Apridian Saputra <saputrapridian07@gmail.com>
2026-02-26 13:16:38 +0700

    feat : edit footer

--- components/user-layout/Footer.tsx
+++ components/user-layout/Footer.tsx
@@ -9,41 +9,42 @@ export default function Footer() {
   return (
     <footer className="w-full flex flex-col items-center justify-center bg-light-grey">
         <Container className="flex flex-col md:flex-row w-full py-8 gap-8 md:gap-12 justify-between text-sm text-gray-700">
-            <div className="flex flex-col gap-3">
-                <Image src="/logo.png" alt="Logo" width={120} height={40} />
-                <p>Telusuri dan pilih produk yang kamu inginkan.</p>
+            <div className="flex flex-col gap-3 justify-around">
+                <div className="flex flex-col gap-4">
+                    <p className="text-sm">Telusuri dan pilih produk yang kamu inginkan.</p>
+                </div>
                 <div className="flex flex-row gap-6">
                     <img src="/icon-twiter.png" alt="Twitter Icon" className="w-6 h-6" />
-                    <img src="/icon-tiktok.png" alt="TikTok Icon" className="w-7 h-7" />
-                    <img src="/icon-instagram.png" alt="Instagram Icon" className="w-7 h-7" />
+                    <img src="/icon-tiktok.png" alt="TikTok Icon" className="w-6 h-6" />
+                    <img src="/icon-instagram.png" alt="Instagram Icon" className="w-6 h-6" />
                 </div>
             </div>  
 
             <div className="flex flex-col md:flex-row justify-between w-full md:w-3/4 gap-8 md:gap-0">
                 <div className="flex flex-col gap-2">
-                    <p className="font-medium pb-1 text-primary-orange">Kategori Populer</p>
+                    <p className="font-semibold pb-1 text-primary-orange text-xs">Kategori Populer</p>
                     {footerCategory.map((item) => (
-                    <Link key={item.href} href={item.href}>
+                    <Link key={item.href} href={item.href} className="hover:text-dark-grey text-xs">
                         {item.title}
                     </Link>
                     ))}
                 </div>
 
                 <div className="flex flex-col gap-2">
-                    <p className="font-medium pb-1 text-primary-orange">Bantuan</p>
+                    <p className="font-semibold pb-1 text-primary-orange text-xs">Bantuan</p>
                     {footerHelp.map((item) => (
-                    <Link key={item.href} href={item.href}>
+                    <Link key={item.href} href={item.href} className="hover:text-dark-grey text-xs">
                         {item.title}
                     </Link>
                     ))}
                 </div>
 
                 <div className="flex flex-col gap-2 max-w-sm">
-                    <p className="font-medium pb-1 text-primary-orange">Dapatkan Info Promo Terbaru</p>
-                    <p className="w-fit">Figma ipsum component variant main layer. Link union invite layout group select.</p>
-                    <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 my-3">
-                        <Input type="search" placeholder="Masukkan Email" className="bg-white w-full sm:w-auto" />
-                        <Button className="w-full sm:w-auto">Sumbit</Button>
+                    <p className="font-semibold pb-1 text-primary-orange text-xs">Dapatkan Info Promo Terbaru</p>
+                    <p className="w-fit text-xs">Figma ipsum component variant main layer. Link union invite layout group select.</p>
+                    <div className="flex items-start sm:items-center gap-2 my-3 w-full">
+                        <Input type="search" placeholder="Masukkan Email" className="bg-white w-full sm:w-auto flex-1" />
+                        <Button className="text-xs">Sumbit</Button>
                     </div>
                 </div>    
             </div>
