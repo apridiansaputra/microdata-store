dd200d2919e65332ef8e6d7ac9fa0ee772c4335a
Apridian Saputra <saputrapridian07@gmail.com>
2026-02-27 22:40:06 +0700

    feat : edit detail product admin

--- app/(admin)/product-admin/[slug]/page.tsx
+++ app/(admin)/product-admin/[slug]/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import Header from "@/components/admin-layout/header"
+import Container from "@/components/admin-layout/container"
 
 export default function ProductDetail() {
   return (
-	<div>ProductDetail</div>
+    <div>
+      <Header title='Produk' />
+
+      <Container className="flex flex-wrap justify-start gap-4">
+          <h1>Detail Produk</h1>
+      </Container>
+    </div>
   )
 }
